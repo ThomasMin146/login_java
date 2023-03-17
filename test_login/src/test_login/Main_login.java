ff6c4bcd3beb960984ee77d7bafae67386664965
Thomas <Thomas@DESKTOP-VO47FHF>
2023-03-17 14:14:15 +0100

    first commit

---
+++ test_login/src/test_login/Main_login.java
@@ -0,0 +1,10 @@
+package test_login;
+
+public class Main_login {
+
+	public static void main(String[] args) {
+		
+		frame_login login = new frame_login();
+	}
+
+}
---
+++ test_login/src/test_login/frame_login.java
@@ -0,0 +1,10 @@
+package test_login;
+
+public class frame_login {
+
+	
+	
+	frame_login(){
+		System.out.println("Test");
+	}
+}
